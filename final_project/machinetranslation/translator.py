a8953ce4c1aa21df1506733954169963d6066666
somebody-45729 <khn91497@gmail.com>
2022-12-30 19:48:48 -0500

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,41 +1,31 @@
-"""
-IMPORTS for project
-"""
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson import LanguageTranslatorV3
-import os
 import json
-from dotenv import load_dotenv
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+# load_dotenv()
 
-load_dotenv()
+# apikey = os.environ['Jke6XZ7cNBND6zVuKAoHLX2xgl0OzHNNByEgZ_zOi0I8']
+# url = os.environ['https://api.us-south.language-translator.watson.cloud.ibm.com/instances/08c7ace4-dd1e-4cfa-bc9f-32ddb90b8f8d']
 
-apikey = os.environ['apikey']
-url = os.environ['url']
 
-#from ibm_watson import LanguageTranslatorV3
-#from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-authenticator = IAMAuthenticator('siQMYfoh1CR0oBvb5Q54Q9CEwVEDwPE3XOALOfVKp4j4')
+# Instance of IBM language
+authenticator = IAMAuthenticator('Jke6XZ7cNBND6zVuKAoHLX2xgl0OzHNNByEgZ_zOi0I8')
 language_translator = LanguageTranslatorV3(
-    version='2022-12-17',
+    version='2022-30-22',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
+language_translator.set_service_url("https://api.us-south.language-translator.watson.cloud.ibm.com/instances/08c7ace4-dd1e-4cfa-bc9f-32ddb90b8f8d")
 
 
-# translating
-def english_to_french(english_text, self):
-    #write the code here
-    translation = self.language_translator.translate(text=english_text,
-            model_id='en-fr').get_result()
-    french_text=translation['translation']['-->']['translation']
+# Function: englishToFrench
+def english_to_french(english_text):
+    translation = language_translator.translate(text=english_text,model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
     return french_text
-
-def french_to_english(french_text, self):
-    #write the code here
-    translation = self.language_translator.translate(
-        text=french_text,
-        model_id='fr-en').get_result()
-    english_text=translation['translation']['-->']['translation']
+    
+# Function: frenchToEnglish
+def french_to_english(french_text):
+    translation = language_translator.translate(text=french_text,model_id='fr-en').get_result()
+    english_text = translation["translations"][0]["translation"]
     return english_text
--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,28 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
+import machinetranslation # Imported
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
+def english_To_French():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    french_text = translator.english_to_french(textToTranslate)
+    return french_text
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+def french_To_English():
+    textToTranslate2 = request.args.get('textToTranslate2')
     # Write your code here
-    return "Translated text to English"
+    english_text = translator.french_to_english(textToTranslate2)
+    return english_text
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
